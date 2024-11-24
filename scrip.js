f12f496709239457fd91444a0fc21f28a8762ffc
Adedayo Ezekiel Adejumo <167407110+Easysoul1@users.noreply.github.com>
2024-11-24 02:56:54 +0100

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,16 +1,23 @@
-
 const inputs = document.querySelectorAll(".css-controller input");
+const resetButton = document.getElementById("reset");
 
+inputs.forEach((input) => input.addEventListener("input", update));
 
-inputs.forEach((inputs)=>inputs.addEventListener("change",update));
-
-
-function update(){
-
-    const suffixData = this.dataset.sizing || "";
-    document.documentElement.style.setProperty(
-        `--${this.name}`,
-        this.value + suffixData
-    );
-
+function update() {
+    const suffix = this.dataset.sizing || "";
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
+
+// Reset all styles to default
+resetButton.addEventListener("click", () => {
+    document.documentElement.style.setProperty("--base", "rgb(255,133,0)");
+    document.documentElement.style.setProperty("--width", "50%");
+    document.documentElement.style.setProperty("--border-radius", "15px");
+    document.documentElement.style.setProperty("--padding", "20px");
+    document.documentElement.style.setProperty("--blur", "0px");
+    document.documentElement.style.setProperty("--rotate", "0deg");
+    document.documentElement.style.setProperty("--opacity", "1");
+    document.documentElement.style.setProperty("--shadow-color", "#000000");
+    document.documentElement.style.setProperty("--shadow-blur", "0px");
+    document.documentElement.style.setProperty("--shadow-spread", "0px");
+});
